ba6331d6e451004cb53f759716e602cbae8d9581
Kevin Oliveira <kevin.oliveira@ecofire.local>
2021-01-26 14:58:13 -0300

    alter icon

--- pages/src/Components/Navbar.js
+++ pages/src/Components/Navbar.js
@@ -13,7 +13,7 @@ function navbar(){
                     <li> <a> <FontAwesomeIcon icon="map-marker"/> locality  </a> </li>
                     <li> <a> <FontAwesomeIcon icon="archive"/> Products  </a> </li>
                     <li> <a> <FontAwesomeIcon icon="fire"/> Hookah  </a> </li>
-                    <li> <a> <FontAwesomeIcon icon="address-card"/> Acount </a> </li>
+                    <li> <a> <FontAwesomeIcon icon="shopping-cart"/> Cart </a> </li>
                     <li> <a> <FontAwesomeIcon icon="user-circle"/> login </a> </li>
                 </ul>
             </nav>
