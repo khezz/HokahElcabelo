ce96990afaf3e49a7e43f6c0ccd4a6d27f3489ba
Kevin Oliveira <kevin.oliveira@ecofire.local>
2021-01-26 16:59:26 -0300

    work HomePage

--- pages/src/pages/Homepage/homepage.js
+++ pages/src/pages/Homepage/homepage.js
@@ -1,13 +1,18 @@
 import Head from '../../Components/Head'
 import Navbar from '../../Components/Navbar'
+import Style from './Homepage.module.css'
 
 function Homepage(){
     return(
         <div>
             <Head title="Hookah-Home"/>
             <Navbar />
-            <div>
+            <div className={Style.title}>
+                <img src="../../midia/img/hookah-logo.jpg"/>
                 <h1>Hookah El cabelo</h1>
+            </div>
+            <div className={Style.container}>
+
             </div>
         </div>
     )
